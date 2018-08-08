6813ba26fd7dfad58ceeb4f238a238d2a59e1a76
MyOreos261 <42008556+MyOreos261@users.noreply.github.com>
2018-08-08 12:44:29 -0400

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,28 @@
+function Home(){
+  document.getElementById("parrotPic").innerHTML=" <img src='G9Nv.gif'><h4>The things that you need to know about Animals!</h4>";
+  document.getElementById("Pics").innerHTML=" <img src='http://wp.patheos.com.s3.amazonaws.com/blogs/wwjtd/files/2014/04/lion-cub.gif'>";
+}
+function insects(){
+  document.getElementById("pics").innerHTML=" <img src='http://gifimage.net/wp-content/uploads/2017/10/bugs-gif-13.gif'>";
+}
+function Reptiles(){
+  document.getElementById("pics").innerHTML=" <img src='https://media0.giphy.com/media/wBShrmQ1FEliw/giphy.gif'>";
+}
+function SeaAnimals(){
+  document.getElementById("pics").innerHTML=" <img src='https://media3.giphy.com/media/KL1juaHQRwblK/giphy.gif'>";
+}
+function Bears(){
+  document.getElementById("pics").innerHTML=" <img src='https://media.giphy.com/media/aK4wh0UE3oddS/giphy.gif'>";
+}
+function Parrots(){
+  document.getElementById("pics").innerHTML=" <img src='https://media2.giphy.com/media/rWqYADRfHCUMw/giphy.gif'>";
+}
+function WildCats(){
+  document.getElementById("pics").innerHTML=" <img src='https://media0.giphy.com/media/H0M1ej8zcEViw/giphy.gif'>";
+}
+function Monkeys(){
+  document.getElementById("pics").innerHTML=" <img src='https://media2.giphy.com/media/TgMEUOhi0ET432dnPH/giphy.gif'>";
+}
+function Penguins(){
+  document.getElementById("pics").innerHTML=" <img src='https://media2.giphy.com/media/aqQsmtq6J2L16/giphy.gif'>";
+}
